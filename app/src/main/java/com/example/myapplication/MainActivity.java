fbf3b567300f6be784e0c8149e38357b815e379e
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-11-16 13:28:38 +0600

    operators

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,7 +7,6 @@ import android.util.Log;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -16,7 +14,8 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
     Observable<String> animalsObservable = Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
     private static final String TAG = "tag";
-
+    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     private Disposable disposable;
 
     @Override
@@ -29,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         animalsObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(animalsObserver);
+
+        Operators operators = new Operators(numbers);
+        operators.rxOperators();
+        operators.setRangeofArray();
+
+        operators.filterData();
 
     }
 
@@ -57,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+
 
     @Override
     protected void onDestroy() {
---
+++ app/src/main/java/com/example/myapplication/Operators.java
@@ -0,0 +1,112 @@
+package com.example.myapplication;
+
+
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class Operators {
+    private Integer[] numbers;
+    private static final String TAG = "tag";
+
+    public Operators(Integer[] numbers) {
+        this.numbers = numbers;
+    }
+
+
+    public void rxOperators() {
+
+        Observable.fromArray(numbers)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Integer>() {
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "Number: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "All number emited");
+                    }
+                });
+    }
+
+    public void setRangeofArray() {
+
+        Observable.range(1, 20)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Integer>() {
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "Number: " + integer);
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "All number emited");
+
+                    }
+                });
+    }
+
+    public void filterData() {
+
+        Observable.range(1, 20)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return false;
+                    }
+                })
+                .map(new Function<Integer, String>() {
+                    @Override
+                    public String apply(Integer integer) throws Exception {
+                        return null;
+                    }
+                })
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.d(TAG, "onNext: " + s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "All numbers emitted!");
+                    }
+                });
+
+    }
+
+}
